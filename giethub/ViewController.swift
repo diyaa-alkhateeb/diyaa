49b7be3279ed69b969f66098d1ae27cb79ee994d
Diyaa Al-khateeb <diyaaal-khateeb@Diyaas-MacBook-Pro.local>
2017-09-21 17:45:23 +0300

    Initial Commit

---
+++ giethub/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  giethub
+//
+//  Created by Diyaa Al-khateeb on 9/21/17.
+//  Copyright © 2017 diyaa alkhateeb. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
